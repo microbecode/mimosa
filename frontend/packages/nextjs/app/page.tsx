9fc17d697669107b7af1bac65a39741fdf8b92c4
Milan Cermak <milan.cermak@gmail.com>
2024-06-21 15:54:51 +0200

    dev(frontend): reading STRK balance from rpc

--- frontend/packages/nextjs/app/page.tsx
+++ frontend/packages/nextjs/app/page.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import type { NextPage } from "next";
 import { Address } from "~~/components/scaffold-stark";
-import { useAccount } from "@starknet-react/core";
+import { useAccount, useBalance } from "@starknet-react/core";
 import { Address as AddressType } from "@starknet-react/chains";
 import { DebugContracts } from "./debug/_components/DebugContracts";
 import {
@@ -13,6 +13,40 @@ import {
 import { useDeployedContractInfo } from "~~/hooks/scaffold-stark/useDeployedContractInfo";
 import { useScaffoldWriteContract } from "~~/hooks/scaffold-stark/useScaffoldWriteContract";
 import { useEffect, useState } from "react";
+
+const strk = "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
+const user1 = "0x64b48806902a367c8598f4f95c305e8c1a1acba5f082d294a43793113115691";
+const user2 = "0x78662e7352d062084b0010068b99288486c2d8b914f6e2a55ce945f8792c8b1";
+
+function BalanceComponent({ address }: { address: string }) {
+  const [balanceState, setBalanceState] = useState<BigInt>();
+  // {
+  //   isLoading: true,
+  //   isError: false,
+  //   error: null,
+  //   data: null
+  // });
+
+  const balanceResult = useBalance({
+    address,
+    token: strk,
+    watch: true
+  });
+
+  useEffect(() => {
+    if (!balanceResult.isLoading) {
+      setBalanceState(balanceResult.data?.value);
+    }
+  });//, [balanceResult.isLoading, balanceResult.isError, balanceResult.data]);
+
+  return <>{ balanceState?.toString() }</>
+
+  // if (balanceState.isLoading) return <div>Loading ... (Address: {address})</div>;
+  // if (balanceState.isError || !balanceState.data)
+  //   return <div>Error: {balanceState.error?.message || 'Unknown error'}</div>;
+
+  // return <div>{balanceState.data.value.toString()} {balanceState.data.symbol}</div>;
+}
 
 const Home: NextPage = () => {
   const connectedAddress = useAccount();
@@ -73,12 +107,6 @@ const Home: NextPage = () => {
     //args: [0, [0], 0]
   });
 
-  // TODO: read balances from the blockchain for real
-  const updateBalance = () => {
-    setUser1Balance("999999999999999999900");
-    setUser2Balance("1000000000000000000100");
-  };
-
   return (
     <>
       <div className="flex items-center flex-col py-8 pt-10">
@@ -135,35 +163,21 @@ const Home: NextPage = () => {
               onClick={() => {
                 withdraw.writeAsync();
               }}
-<<<<<<< HEAD
               className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
               Withdraw from Mimosa
             </div>
-=======
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500">Withdraw from Mimosa</div>
->>>>>>> 14e793c (fix(frontend): button label)
           </div>
         </div>
       }
 
       {
-        <div className="flex flex-col items-center w-full">
-          <p className="mb-2 text-center text-black">
-            User 1 STRK balance: {user1Balance}
-          </p>
-          <p className="text-center text-black">
-            User 2 STRK balance: {user2Balance}
-          </p>
-
-          <div
-            onClick={updateBalance}
-            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            Update balances
-          </div>
-        </div>
+      <div className="flex flex-col items-center w-full">
+        <p className="mb-2 text-center text-black">User 1 STRK: <BalanceComponent address={user1} /></p>
+        <p className="mb-2 text-center text-black">User 2 STRK: <BalanceComponent address={user2} /></p>
+      </div>
       }
+
     </>
   );
 };
