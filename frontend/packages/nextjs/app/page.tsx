007578a21a35b82d1172b2447b7e460dedfbfcf4
Milan Cermak <milan.cermak@gmail.com>
2024-06-21 15:56:04 +0200

    style(frontend): label placeholders

--- frontend/packages/nextjs/app/page.tsx
+++ frontend/packages/nextjs/app/page.tsx
@@ -127,7 +127,7 @@ const Home: NextPage = () => {
               value={commitmentValue}
               onChange={handleCommitmentChange}
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Commitment (felt252)"
+              placeholder="Secret (felt252)"
             />
             <div
               onClick={() => {
@@ -157,7 +157,7 @@ const Home: NextPage = () => {
               value={withdrawPreimageValue}
               onChange={handleWithdrawPreimageChange}
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Preimage (felt252)"
+              placeholder="Secret (felt252)"
             />
             <div
               onClick={() => {
